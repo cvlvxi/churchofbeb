1ff9600ff0a6b03e508a376bce06758fc727567e
cvlvxi <cvlvxi@outlook.com>
2022-01-04 18:41:49 +1100

    contain stuff

--- app/src/main/java/com/cvlvxi/churchofbeb/LotteAnimation.kt
+++ app/src/main/java/com/cvlvxi/churchofbeb/LotteAnimation.kt
@@ -4,108 +4,129 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.*
 import kotlin.random.Random
 
-
-
 data class DirectionalLottie(
     val lottieId: Int,
     val imageIsLeft: Boolean,
 )
 
-data class movingLottieContainer(
+data class MovingLottieContainer(
     val lottieId: Int,
     val imageIsLeft: Boolean,
     var start: Float,
     var end: Float,
     var walkingDuration: Int
 ) {
-    var prevOffset: Float? = null
+  var prevOffset: Float? = null
 
-    fun shouldMirror(newOffsetX: Float): Boolean {
-        val isRight = (prevOffset?: start) < newOffsetX
-        return imageIsLeft == isRight
-    }
+  fun shouldMirror(newOffsetX: Float): Boolean {
+    val isRight = (prevOffset ?: start) < newOffsetX
+    return imageIsLeft == isRight
+  }
 }
 
 @Composable
-fun SimpleLottie(
+fun SimpleLottie(lottieId: Int, modifier: Modifier, size: Dp = 100.dp) {
+  val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottieId))
+  LottieAnimation(
+      composition, iterations = LottieConstants.IterateForever, modifier = modifier.size(size))
+}
+
+data class TransitionAlphaLottieContainer(
+    val lottieId: Int,
+    val size: Dp,
+    val alphaStart: Float,
+    val alphaEnd: Float,
+    val condition: Boolean,
+    val spec: AnimationSpec<Float>? =
+        TweenSpec(durationMillis = 3000, delay = 0, easing = LinearEasing),
+    val modifier: Modifier?,
+)
+
+@Composable
+fun TransitionAlphaSimpleLottie(
     lottieId: Int,
+    size: Dp = 100.dp,
+    alphaStart: Float,
+    alphaEnd: Float,
+    condition: Boolean,
+    spec: AnimationSpec<Float> =
+        TweenSpec<Float>(durationMillis = 3000, delay = 0, easing = LinearEasing),
     modifier: Modifier,
-    size: Dp = 100.dp
 ) {
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottieId))
+  val alpha: Float by
+      animateFloatAsState(if (!condition) alphaStart else alphaEnd, animationSpec = spec)
+  SimpleLottie(lottieId = lottieId, size = size, modifier = modifier.alpha(alpha))
+}
+
+@Composable
+fun TransitionBetweenTwoOverlappingLotties(
+    transAlphaLottie1: TransitionAlphaLottieContainer,
+    transAlphaLottie2: TransitionAlphaLottieContainer
+) {}
+
+fun generateLotties(
+    lottiePool: List<DirectionalLottie>,
+    howMany: Int,
+    boxMaxWidth: Dp
+): List<MovingLottieContainer> {
+  val maxWidth = boxMaxWidth.value.toInt()
+  val halfMaxWidth = maxWidth / 2
+
+  val lotties = mutableListOf<MovingLottieContainer>()
+  (0 until howMany).forEach { _ ->
+    val start = Random.nextInt(0, maxWidth)
+    val end = if (start < halfMaxWidth) (maxWidth + start) else (0 - start)
+    val randIdx = Random.nextInt(0, lottiePool.size)
+    lotties.add(
+        MovingLottieContainer(
+            lottieId = lottiePool[randIdx].lottieId,
+            imageIsLeft = lottiePool[randIdx].imageIsLeft,
+            start = start.toFloat(),
+            end = end.toFloat(),
+            walkingDuration = Random.nextInt(5, 30) * 1000,
+        ))
+  }
+  return lotties
+}
+
+@Composable
+fun MovingLotties(
+    shouldSuspend: Boolean,
+    lotties: List<MovingLottieContainer>,
+    modifier: Modifier
+) {
+  lotties.forEach { lottie ->
+    val specInfinite =
+        InfiniteRepeatableSpec<Float>(
+            animation =
+                tween(
+                    durationMillis = lottie.walkingDuration,
+                    delayMillis = 0,
+                    easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse)
+    val specFinite = TweenSpec<Float>(durationMillis = 1000, delay = 0, easing = LinearEasing)
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottie.lottieId))
+    val offset = remember { Animatable(lottie.start) }
+
+    LaunchedEffect(shouldSuspend) {
+      offset.animateTo(lottie.end, animationSpec = if (!shouldSuspend) specInfinite else specFinite)
+    }
+
     LottieAnimation(
         composition,
         iterations = LottieConstants.IterateForever,
-        modifier = modifier
-            .size(size)
-    )
-}
-
-fun generateLotties(lottiePool: List<DirectionalLottie>, howMany: Int, boxMaxWidth: Dp): List<movingLottieContainer> {
-    val maxWidth = boxMaxWidth.value.toInt()
-    val halfMaxWidth = maxWidth / 2
-
-
-    val lotties = mutableListOf<movingLottieContainer>()
-    (0 until howMany).forEach { _ ->
-        val start = Random.nextInt(0, maxWidth)
-        val end = if (start < halfMaxWidth) (maxWidth+start) else (0 - start)
-        val randIdx = Random.nextInt(0, lottiePool.size)
-        lotties.add(movingLottieContainer(
-            lottieId=lottiePool[randIdx].lottieId,
-            imageIsLeft=lottiePool[randIdx].imageIsLeft,
-            start=start.toFloat(),
-            end=end.toFloat(),
-            walkingDuration = Random.nextInt(5,30) * 1000,
-        ))
-    }
-    return lotties
-}
-
-
-
-
-@Composable
-fun MoveXLotties(shouldSuspend: Boolean, lotties: List<movingLottieContainer>, modifier: Modifier) {
-    lotties.forEach { lottie ->
-        val specInfinite = InfiniteRepeatableSpec<Float>(
-            animation = tween(
-                durationMillis=lottie.walkingDuration,
-                delayMillis = 0,
-                easing = LinearEasing
-            ),
-            repeatMode = RepeatMode.Reverse
-        )
-        val specFinite = TweenSpec<Float>(
-            durationMillis=1000,
-            delay = 0,
-            easing = LinearEasing
-        )
-        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottie.lottieId))
-        val offset = remember { Animatable(lottie.start) }
-
-        LaunchedEffect(shouldSuspend) {
-            offset.animateTo(
-                lottie.end,
-                animationSpec = if (!shouldSuspend) specInfinite else specFinite
-            )
-        }
-
-        LottieAnimation(
-            composition,
-            iterations = LottieConstants.IterateForever,
-            modifier = modifier
+        modifier =
+            modifier
                 .size(100.dp)
                 .offset(x = offset.value.dp)
-                .scale(scaleX = if (lottie.shouldMirror(offset.value)) -1f else 1f, scaleY = 1f)
-        )
-        lottie.prevOffset = offset.value
-    }
+                .scale(scaleX = if (lottie.shouldMirror(offset.value)) -1f else 1f, scaleY = 1f))
+    lottie.prevOffset = offset.value
+  }
 }
-
--- app/src/main/java/com/cvlvxi/churchofbeb/areas/ChurchEntrance.kt
+++ app/src/main/java/com/cvlvxi/churchofbeb/areas/ChurchEntrance.kt
@@ -2,8 +2,8 @@ package com.cvlvxi.churchofbeb.areas
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.TweenSpec
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -24,127 +24,127 @@ import com.cvlvxi.churchofbeb.R
 
 @Composable
 fun FlyingLotties(shouldSuspend: Boolean, modifier: Modifier, maxWidth: Dp) {
-    val lottiePool= listOf(
-        DirectionalLottie(R.raw.birds_flying1, false)
-    )
-    val lotties = generateLotties(lottiePool, 2,  maxWidth)
-    MoveXLotties(shouldSuspend, lotties, modifier = modifier)
+  val lottiePool = listOf(DirectionalLottie(R.raw.birds_flying1, false))
+  val lotties = generateLotties(lottiePool, 2, maxWidth)
+  MovingLotties(shouldSuspend, lotties, modifier = modifier)
 }
-
 
 @Composable
 fun RunWalkingLotties(shouldSuspend: Boolean, numLotties: Int, modifier: Modifier, maxWidth: Dp) {
-    val lottiePool= listOf(
-        DirectionalLottie(R.raw.walking_cat1, false),
-        DirectionalLottie(R.raw.walking_girl1, false),
-        DirectionalLottie(R.raw.walking_girl2, false),
-        DirectionalLottie(R.raw.walking_girl3, true),
-        DirectionalLottie(R.raw.walking_guy1, false),
-        DirectionalLottie(R.raw.walking_guy2, false),
-        DirectionalLottie(R.raw.walking_guy3, true),
-        DirectionalLottie(R.raw.walking_duck1, false),
-        DirectionalLottie(R.raw.walking_orange1, false),
-        DirectionalLottie(R.raw.walking_peach1, false),
-    )
-    val lotties = generateLotties(lottiePool, numLotties,  maxWidth)
-    MoveXLotties(shouldSuspend, lotties, modifier = modifier)
+  val lottiePool =
+      listOf(
+          DirectionalLottie(R.raw.walking_cat1, false),
+          DirectionalLottie(R.raw.walking_girl1, false),
+          DirectionalLottie(R.raw.walking_girl2, false),
+          DirectionalLottie(R.raw.walking_girl3, true),
+          DirectionalLottie(R.raw.walking_guy1, false),
+          DirectionalLottie(R.raw.walking_guy2, false),
+          DirectionalLottie(R.raw.walking_guy3, true),
+          DirectionalLottie(R.raw.walking_duck1, false),
+          DirectionalLottie(R.raw.walking_orange1, false),
+          DirectionalLottie(R.raw.walking_peach1, false),
+      )
+  val lotties = generateLotties(lottiePool, numLotties, maxWidth)
+  MovingLotties(shouldSuspend, lotties, modifier = modifier)
 }
 
 @Composable
 fun ChurchEntrance() {
-    var nightMode by remember { mutableStateOf(false) }
-    Text(text = "dog")
-    ScaffoldComposable(
-        nightMode = nightMode,
-        toggleNightMode = { nightMode = !nightMode },
-    )
+  var nightMode by remember { mutableStateOf(false) }
+  Text(text = "dog")
+  ScaffoldComposable(
+      nightMode = nightMode,
+      toggleNightMode = { nightMode = !nightMode },
+  )
 }
 
-@Composable
-fun TopMenu(modifier: Modifier = Modifier.height(10.dp)) {
-    Text("Welcome to the Church of the Beb")
-}
+// @Composable
+// fun TopMenu(modifier: Modifier = Modifier.height(10.dp)) {
+//    Text("Welcome to the Church of the Beb")
+// }
 
 @Composable
 fun ScaffoldComposable(nightMode: Boolean, toggleNightMode: () -> Unit) {
-    //  Scaffold(topBar = { TopMenu() }) {
+  //  Scaffold(topBar = { TopMenu() }) {
 
-    Scaffold() {
-        val entrance = painterResource(id = R.drawable.entrance)
-        val bebface = painterResource(id = R.drawable.bebface)
-        val nightMode2 by rememberUpdatedState(nightMode)
+  Scaffold() {
+    val entrance = painterResource(id = R.drawable.entrance)
+    val bebface = painterResource(id = R.drawable.bebface)
+    val nightMode2 by rememberUpdatedState(nightMode)
 
-        BoxWithConstraints {
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop,
-                painter = entrance,
-                contentDescription = "Entrance")
+    BoxWithConstraints {
+      val SimpleFloatTween =
+          TweenSpec<Float>(durationMillis = 3000, delay = 0, easing = LinearEasing)
+      val SimpleColorTween =
+          TweenSpec<Color>(durationMillis = 3000, delay = 0, easing = LinearEasing)
+      val dayTime = Color.Yellow
+      val nightTime = Color.Black
+      val dayToNightAlpha: Float by
+          animateFloatAsState(if (!nightMode) 0.1f else 0.8f, animationSpec = SimpleFloatTween)
+      val bebFaceAlpha: Float by
+          animateFloatAsState(if (!nightMode) 0.7f else 0.8f, animationSpec = SimpleFloatTween)
+      val backgroundColor by
+          animateColorAsState(
+              if (nightMode) nightTime else dayTime, animationSpec = SimpleColorTween)
 
-            FlyingLotties(nightMode2, modifier = Modifier.align(Alignment.TopStart), maxWidth = maxWidth)
+      Image(
+          modifier = Modifier.fillMaxSize(),
+          contentScale = ContentScale.Crop,
+          painter = entrance,
+          contentDescription = "Entrance")
 
-            SimpleLottie(
-                lottieId = R.raw.door_sign1,
-                size = 50.dp,
-                modifier = Modifier.align(Alignment.Center).offset(y = 200.dp, x = -30.dp),
-            )
-            RunWalkingLotties(
-                nightMode2, numLotties = 8, modifier = Modifier.align(Alignment.BottomStart), maxWidth = maxWidth)
+      FlyingLotties(nightMode2, modifier = Modifier.align(Alignment.TopStart), maxWidth = maxWidth)
 
-            val dayTime = Color.Yellow
+      Box(modifier = Modifier.align(Alignment.Center).offset(y = 200.dp, x = -30.dp)) {
+        TransitionAlphaSimpleLottie(
+            lottieId = R.raw.door_sign1,
+            size = 50.dp,
+            alphaStart = 1.0f,
+            alphaEnd = 0.0f,
+            condition = nightMode,
+            modifier = Modifier)
+        TransitionAlphaSimpleLottie(
+            lottieId = R.raw.closed_sign1,
+            size = 50.dp,
+            alphaStart = 0.0f,
+            alphaEnd = 1.0f,
+            condition = nightMode,
+            modifier = Modifier)
+      }
 
-            val nightTime = Color.Black
+      RunWalkingLotties(
+          nightMode2,
+          numLotties = 8,
+          modifier = Modifier.align(Alignment.BottomStart),
+          maxWidth = maxWidth)
 
-            val sunMoonAlpha: Float by
-            animateFloatAsState(
-                if (!nightMode) 0.1f else 0.8f,
-                animationSpec = tween(durationMillis = 3000, delayMillis = 0, easing = LinearEasing)
-            )
-            val bebFaceAlpha: Float by
-            animateFloatAsState(
-                if (!nightMode) 0.7f else 0.8f,
-                animationSpec = tween(durationMillis = 3000, delayMillis = 0, easing = LinearEasing)
-            )
+      Canvas(modifier = Modifier.fillMaxSize()) {
+        drawRect(backgroundColor, alpha = dayToNightAlpha)
+      }
 
-
-            val backgroundColor by
-            animateColorAsState(
-                if (nightMode) nightTime else dayTime,
-                animationSpec = tween(durationMillis = 3000, delayMillis = 0, easing = LinearEasing)
-            )
-
-            Canvas(modifier = Modifier.fillMaxSize()) { drawRect(backgroundColor, alpha = sunMoonAlpha) }
-
-            Box(modifier = Modifier.align(Alignment.TopEnd).offset(x = 50.dp, y = (-50).dp)) {
-                val sunAlpha: Float by
-                animateFloatAsState(
-                    if (!nightMode) 0.3f else 0.0f,
-                    animationSpec =
-                    tween(durationMillis = 3000, delayMillis = 0, easing = LinearEasing)
-                )
-                val moonAlpha: Float by
-                animateFloatAsState(
-                    if (!nightMode) 0.0f else 0.5f,
-                    animationSpec =
-                    tween(durationMillis = 3000, delayMillis = 0, easing = LinearEasing)
-                )
-
-                SimpleLottie(
-                    lottieId = R.raw.sun1,
-                    size = 200.dp,
-                    modifier = Modifier.align(Alignment.Center).alpha(sunAlpha))
-                SimpleLottie(
-                    lottieId = R.raw.moon3,
-                    size = 250.dp,
-                    modifier = Modifier.align(Alignment.Center).alpha(moonAlpha))
-                Image(
-                    painter = bebface,
-                    contentDescription = "BebFace",
-                    modifier =
-                    Modifier.align(Alignment.Center).alpha(bebFaceAlpha).size(70.dp).clickable {
-                        toggleNightMode()
-                    })
-            }
-        }
+      Box(modifier = Modifier.align(Alignment.TopEnd).offset(x = 50.dp, y = (-50).dp)) {
+        TransitionAlphaSimpleLottie(
+            lottieId = R.raw.sun1,
+            size = 200.dp,
+            alphaStart = 0.3f,
+            alphaEnd = 0.0f,
+            condition = nightMode,
+            modifier = Modifier.align(Alignment.Center))
+        TransitionAlphaSimpleLottie(
+            lottieId = R.raw.moon3,
+            size = 250.dp,
+            alphaStart = 0.0f,
+            alphaEnd = 0.5f,
+            condition = nightMode,
+            modifier = Modifier.align(Alignment.Center))
+        Image(
+            painter = bebface,
+            contentDescription = "BebFace",
+            modifier =
+                Modifier.align(Alignment.Center).alpha(bebFaceAlpha).size(70.dp).clickable {
+                  toggleNightMode()
+                })
+      }
     }
+  }
 }
